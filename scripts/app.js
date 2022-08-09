fc095b4ad9e75ec187f185adffed3e6f1befaa8d
niel <53465699+el-niel@users.noreply.github.com>
2022-08-09 16:41:59 +0100

    updated the weather app: to display icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,43 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector(".time");
+const icon = document.querySelector(".icons img");
 
 const updateUI = (data) => {
-  const cityDets = data.cityDetails;
-  const weather = data.weather;
+  const { cityDetails, weather } = data;
 
   // update the html template updateUI
   details.innerHTML = `
-     <h3 class="card-title">${cityDets.EnglishName}</h3>
+     <h3 class="card-title">${cityDetails.EnglishName}</h3>
             <p class="card-text">${weather.WeatherText}</p>
             <h2 class="display-3">${weather.Temperature.Metric.Value}<span>&degC</span></h2>
            
   
-  
+
   `;
+
+  // update the weather icon by numbers
+  let iconSrc = `/img/icons/${weather.WeatherIcon}.svg`;
+
+  icon.setAttribute("src", iconSrc);
+
+  // update image for Day and Night
+  let timeZone = null;
+  if (weather.IsDayTime) {
+    timeZone = "/img/day.svg";
+  } else {
+    timeZone = "/img/night.svg";
+  }
+
+  time.setAttribute("src", timeZone);
 
   // remove the d-none
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
   }
 };
+
 // function for updating city
 const updateCity = async (city) => {
   const cityDetails = await getCity(city);
